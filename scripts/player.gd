c95ac09afea014e720c7214791b7f2211fcf0aa7
Robert Aron Zimmermann <rraz639@gmail.com>
2025-12-28 23:02:17 -0300

    Add blocks with textures

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export var playerSpeed = 8.0
+@export var playerSpeed = 2.0
 @export var playerAcceleration = 5.0
 @export var cameraSensitivity = 0.25
 @export var cameraAcceleration = 2.0
